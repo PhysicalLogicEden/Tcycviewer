a4409cc4ef22c5d9f88ce98869e93f3088843ab2
edeneilie <86970962+edeneilie@users.noreply.github.com>
2021-07-15 09:48:29 +0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,12 +32,12 @@ if uploaded_file is not None:
     st.write(file_details)
 	# Check File Type
     if uploaded_file.type == "text/plain":
-        # df = pd.read_csv(uploaded_file,delimiter='\t')
-        # st.write(df[1:6,:])
+        df = pd.read_table(uploaded_file,delimiter='\t')
+        st.write(df[12,0])
         devNameInd = uploaded_file.name.find('L1')
         deviceName  = uploaded_file.name[devNameInd:devNameInd+14]
         DevicePath = os.path.join(Analyzed_folder_path, deviceName)
-        st.write(DevicePath,deviceName)
+        st.write(DevicePath)
                       # if not os.path.isdir(DevicePath):
                       #         os.mkdir(DevicePath) 
                       #         save_uploadedfile(uploaded_file,deviceName)
