98bd7eb06ce6b4e01c1f2d296dbb6ef72b7f455e
edeneilie <86970962+edeneilie@users.noreply.github.com>
2021-07-15 09:26:34 +0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ Created on Wed Jul 14 09:07:57 2021
 #pip install streamlit
 
 import streamlit as st
-
+import pandas as pd
    
 # import PL own function from path
 # from TcyclesAnalyzerA01 import TcyclesAnalyzer
@@ -28,12 +28,15 @@ def save_uploadedfile(uploadedfile,deviceName):
     
 uploaded_file = st.file_uploader("Upload File",type=['txt']) 
 if uploaded_file is not None:
-   if uploaded_file.type == "text/plain":
+   if uploaded_file.type == "txt":
       file_details = {"Filename":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
+      df = pd.read_csv(uploaded_file,delimiter='\t')
+      st.write(df[1:6,:])
       devNameInd = uploaded_file.name.find('L1')
       deviceName  = uploaded_file.name[devNameInd:devNameInd+14]
       DevicePath = os.path.join(Analyzed_folder_path, deviceName)
-      if not os.path.isdir(DevicePath):
-              os.mkdir(DevicePath) 
-              save_uploadedfile(uploaded_file,deviceName)
+      st.write(DevicePath)
+      # if not os.path.isdir(DevicePath):
+      #         os.mkdir(DevicePath) 
+      #         save_uploadedfile(uploaded_file,deviceName)
       # plot_folder =  TcyclesAnalyzer(uploaded_file.name,uploaded_file)
