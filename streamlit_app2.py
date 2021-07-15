8372281051934b7e052933bf090b2496effb6d68
edeneilie <86970962+edeneilie@users.noreply.github.com>
2021-07-15 17:24:05 +0300

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -108,7 +108,7 @@ if uploaded_file is not None:
                 startEnd4p.append([midNeg4p-int(meanArea*2.5)-2*delta,midNeg4p+int(1.5*meanArea)+4*delta])
         else:
             grad =np.gradient(dataTcyc[:,0])
-            st.write(grad)
+            st.write(grad[15])
             #clustering grad into 2 clusters to find indForCalc 
             kmeans = KMeans(init = 'k-means++',n_clusters = 2)
             indForCalc_y_kmeans = kmeans.fit_predict(abs(grad).reshape(-1,1)) 
