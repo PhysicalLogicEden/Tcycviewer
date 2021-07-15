573974cfeb0770b43c1718de34b47f0c1d87baac
edeneilie <86970962+edeneilie@users.noreply.github.com>
2021-07-15 16:15:59 +0300

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -84,7 +84,7 @@ if uploaded_file is not None:
         if('cont' in filename):
             cont = True
         delta = 10
-        st.write(type(dataTcyc))
+        st.write(dataTcyc[:3,:])
         Neg4p = np.where(dataTcyc[:,0]<0.8*np.min(dataTcyc[:,0]))[0]#finds indexes of the -1g areas for all 4p
             #filtering outliers:
         clf = LocalOutlierFactor(n_neighbors=10)
