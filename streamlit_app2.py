9be56ad3e7cea54e140e8926505247bcddb99075
edeneilie <86970962+edeneilie@users.noreply.github.com>
2021-07-15 16:24:38 +0300

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -84,7 +84,8 @@ if uploaded_file is not None:
         if('cont' in filename):
             cont = True
         delta = 10
-        st.write(dataTcyc[:3,:])
+        st.write(type(dataTcyc[0,0])
+        st.write(dataTcyc[:3,:])       #V
         v = dataTcyc.astype(np.float)
         st.write(type(v[0,0]))
         Neg4p = np.where(dataTcyc[:,0]<0.8*np.min(dataTcyc[:,0]))[0]#finds indexes of the -1g areas for all 4p
