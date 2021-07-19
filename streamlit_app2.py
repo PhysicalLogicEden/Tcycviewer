f2eb10f27b8f4a5e534f8410bc1e380cb459c8c1
edeneilie <86970962+edeneilie@users.noreply.github.com>
2021-07-19 13:01:28 +0300

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -129,6 +129,7 @@ if uploaded_file is not None:
                 startEnd4p.append([indexesOf4Ps[i][0]-2*delta,indexesOf4Ps[i][-1]+2*delta])
         startEnd4p.sort()
         startEnd4p[-1][1] = min(startEnd4p[-1][1],len(dataTcyc))
+        st.write(startEnd4p[-1][1])
         #########creates allfpa and indexes
         gradIndexes = []
         Indexes = []
